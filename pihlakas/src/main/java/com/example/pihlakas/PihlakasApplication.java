346322facf9eaddc4c07815a1b862efc178d276b
EllenLo <elloit@ttu.ee>
2018-02-19 14:32:04 +0200

    springboot
    projekti algus

---
+++ pihlakas/src/main/java/com/example/pihlakas/PihlakasApplication.java
@@ -0,0 +1,12 @@
+package com.example.pihlakas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PihlakasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PihlakasApplication.class, args);
+	}
+}
---
+++ pihlakas/src/test/java/com/example/pihlakas/PihlakasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.pihlakas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PihlakasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
