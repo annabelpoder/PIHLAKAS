cddc9b10935775898fb02fc9eef1cd5d85881163
EllenLo <elloit@ttu.ee>
2018-02-20 19:11:59 +0200

    Revert "aurelia muudatused"
    This reverts commit 9fe77031215194b6345e153cae1b5993305f4ae1.

--- pihlakasAU/src/app.js
+++ pihlakasAU/src/app.js
@@ -2,12 +2,4 @@ export class Hello {
   constructor() {
     this.name = 'Külastaja';
   }
-
-  configureRouter(config, router){
-  	this.router = router;
-  	config.title = 'Disc golfi veebirakendus';
-  	config.map({
-  		{ route: ['', 'home'], name: 'home', moduleId:'home/index'}
-  	});
-  }
 }
