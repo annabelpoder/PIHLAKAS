9fe77031215194b6345e153cae1b5993305f4ae1
EllenLo <elloit@ttu.ee>
2018-02-20 14:18:53 +0200

    aurelia muudatused

--- pihlakasAU/src/app.js
+++ pihlakasAU/src/app.js
@@ -2,4 +2,12 @@ export class Hello {
   constructor() {
     this.name = 'Külastaja';
   }
+
+  configureRouter(config, router){
+  	this.router = router;
+  	config.title = 'Disc golfi veebirakendus';
+  	config.map({
+  		{ route: ['', 'home'], name: 'home', moduleId:'home/index'}
+  	});
+  }
 }
