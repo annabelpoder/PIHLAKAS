0ad948c48f9c31d294e5af7bc0c3f383a8de14c3
EllenLo <elloit@ttu.ee>
2018-04-04 20:09:15 +0300

    Initial commit

---
+++ src/main/java/com/example/pihlakasspring/PihlakasSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.pihlakasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PihlakasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PihlakasSpringApplication.class, args);
+	}
+}
