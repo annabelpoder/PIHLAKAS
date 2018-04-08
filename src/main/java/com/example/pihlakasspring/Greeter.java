df39f0221200cd4f12cc48ef4324be9b97bfcd4a
EllenLo <elloit@ttu.ee>
2018-04-08 17:39:08 +0300

    fresh start

--- src/main/java/com/example/pihlakasspring/Greeter.java
+++
@@ -1,7 +0,0 @@
-package com.example.pihlakasspring;
-
-public class Greeter {
-    public String sayHello(){
-        return "Hello world!";
-    }
-}
--- src/main/java/com/example/pihlakasspring/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.example.pihlakasspring;
-
-public class HelloWorld
-{
-    public static void main (String[] args){
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-    }
-}
