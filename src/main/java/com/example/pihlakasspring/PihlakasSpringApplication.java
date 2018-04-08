31eacea07900b0b5388e96cf09128b750479d5f3
EllenLo <elloit@ttu.ee>
2018-04-08 17:35:22 +0300

    Spring bootiga katsetamine (REST) v3

--- src/main/java/com/example/pihlakasspring/PihlakasSpringApplication.java
+++ src/main/java/com/example/pihlakasspring/PihlakasSpringApplication.java
@@ -1,18 +1,12 @@
 package com.example.pihlakasspring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class PihlakasSpringApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(SpringBootApplication.class);
 	public static void main(String[] args) {
-		RestTemplate restTemplate = new RestTemplate();
-		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-		log.info(quote.toString());
+		new SpringApplicationBuilder(PihlakasSpringApplication.class).build().run(args);
 	}
 }
