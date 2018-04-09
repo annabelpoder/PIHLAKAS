e0cf3baf92e3ec0cee5990185bada42f50d10ca2
Carmen <miller.carmen96@gmail.com>
2018-04-09 23:48:18 +0300

    sql script

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO track (id, name, available) VALUES (1, 'Nõmme KEEN discgolfipark', true);
+INSERT INTO track (id, name, available) VALUES (2, 'Maarjamäe discgolfipark', true);
+INSERT INTO track (id, name, available) VALUES (3, 'Männiku discgolfipark', true);
