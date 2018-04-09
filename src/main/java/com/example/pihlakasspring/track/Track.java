3af99140c7cbfc113a6fad96e9ac222634ef8561
Carmen <miller.carmen96@gmail.com>
2018-04-09 18:55:54 +0300

    MariaDB

--- src/main/java/com/example/pihlakasspring/track/Track.java
+++ src/main/java/com/example/pihlakasspring/track/Track.java
@@ -1,23 +1,31 @@
 package com.example.pihlakasspring.track;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
 public class Track
 {
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private boolean available;
 
     public Long getId(){return id;}
-    public void setId(Long id) {this.id = id;}
+    void setId(Long id) {this.id = id;}
     public String getName(){
         return name;
     }
-    public void setName(String name){
+    void setName(String name){
         this.name = name;
     }
     public boolean isAvailable(){
         return available;
     }
-    public void setAvailable (boolean available){
+    void setAvailable(boolean available){
         this.available = available;
     }
 }
