ba33a128109b7a8b026b9f742cdd51e9912b3531
EllenLo <elloit@ttu.ee>
2018-05-15 02:08:56 +0300

    delete

--- src/main/java/com/example/pihlakasspring/player/Player.java
+++ src/main/java/com/example/pihlakasspring/player/Player.java
@@ -18,6 +18,47 @@ public class Player {
     @Id
     @GeneratedValue
     Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public double getShotsNr() {
+        return shotsNr;
+    }
+
+    public void setShotsNr(double shotsNr) {
+        this.shotsNr = shotsNr;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    public void setPoints(double points) {
+        this.points = points;
+    }
+
     String firstName;
     String trackName;
     double shotsNr;
