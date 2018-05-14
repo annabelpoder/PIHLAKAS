eda76ea337d23a581413d29e6d1689dbb702b738
EllenLo <elloit@ttu.ee>
2018-05-15 01:38:04 +0300

    delete

--- src/main/java/com/example/pihlakasspring/track/NewTrackJson.java
+++
@@ -1,9 +0,0 @@
-package com.example.pihlakasspring.track;
-
-public class NewTrackJson {
-
-    private  String name;
-
-    public String getName(){return name;}
-    public void setName(String name){this.name = name;}
-}
